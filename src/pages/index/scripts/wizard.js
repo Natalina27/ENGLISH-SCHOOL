cd72d46158db65d8b38d20129e3c4e3942298a11
Natalya Myunster <natalyamyunster@yahoo.com>
2020-11-25 14:23:07 +0100

    task_2

--- src/pages/index/scripts/wizard.js
+++ src/pages/index/scripts/wizard.js
@@ -1,4 +1,8 @@
+import {saveToStorage} from './login';
+
 console.log('wizard');
+
+const obj = {};
 
 const getElById = (el) => document.getElementById(el);
 const styleDisNone = (el) => el.style.display = 'none';
@@ -26,6 +30,20 @@ const studentFormBlock = getElById('step2Block');
 //Radio
 const student = getElById('user_student');
 
+//Form
+const formLogin = regBlock.querySelector('.form-login');
+const nameInput = getElById('name');
+const mailInput = getElById('email');
+const passwordInput = getElById('password');
+const confirmPasswordInput = getElById('password_next');
+const createAccountBtn = getElById('createAccount');
+const clearForm = () => {
+  nameInput.value = '';
+  mailInput.value = '';
+  passwordInput.value = '';
+  confirmPasswordInput.value = '';
+};
+
 //Functions
 const wruBlockOpen = () => {
   styleDisBlock(wruBlock);
@@ -49,7 +67,84 @@ const studentFormOpen = () => {
   styleDisNone(wruBlock);
 };
 const continueByType = () => {
-  student.checked ? studentFormOpen() : regBlockOpen();
+  if (student.checked) {
+    studentFormOpen();
+    obj.type = 'student';
+  } else {
+    regBlockOpen();
+    obj.type = 'teacher';
+  }
+};
+const generateError = (el) => {
+  el.className = 'error-reg';
+  return false;
+};
+const removeValidation = () => {
+  const errors = formLogin.querySelectorAll('.error-reg');
+  errors.forEach(item => {
+    item.classList.remove('error-reg');
+    item.classList.add('input-box');
+  });
+
+};
+
+const fields = formLogin.querySelectorAll('.input-box');
+
+const checkFieldsNotEmpty = () => {
+  let response = true;
+  fields.forEach(item => {
+    if (!item.value) {
+      const error = generateError(item, ' Cannot be blank');
+      console.error('Cannot be blank');
+      response = error;
+    }
+  });
+  return response;
+};
+
+const nameValidation = () => {
+  const name = nameInput.value.split(' ');
+  if (name.length === 2 && name.every(el => el.length >= 3)) {
+    return true;
+  } else {
+    generateError(nameInput);
+  }
+};
+const checkPasswordMatch = () => {
+  if (passwordInput.value !== confirmPasswordInput.value) {
+    console.error('passwords are not equals');
+    generateError(confirmPasswordInput, 'Password doesnt match');
+    return false;
+  } else {
+    return true;
+  }
+};
+const formLoginSubmit = () => {
+  removeValidation();
+
+  obj.name = nameInput.value;
+  obj.email = mailInput.value;
+  obj.password = passwordInput.value;
+  obj.confirmPassword = confirmPasswordInput.value;
+
+  const notEmpty = checkFieldsNotEmpty();
+  const passwordMatch = checkPasswordMatch();
+  const nameValid = nameValidation();
+
+  const students = JSON.parse(localStorage.getItem('students')) || [];
+
+  if (notEmpty && passwordMatch && nameValid) {
+    if (obj.type === 'teacher') {
+      localStorage.setItem('teacher', JSON.stringify(obj));
+      saveToStorage(obj);
+      window.location.href = 'teacher.html';
+    } else {
+      localStorage.setItem('students', JSON.stringify([obj, ...students]));
+      saveToStorage(obj);
+      window.location.href = 'student.html';
+    }
+  }
+  clearForm();
 };
 
 btnClick(regBtn, wruBlockOpen);
@@ -62,3 +157,4 @@ btnClick(from1to2Btn, continueByType);
 btnClick(toLoginBtn, loginBlockOpen);
 btnClick(continueStudentBtn, regBlockOpen);
 btnClick(from4to5Btn, regBlockOpen);
+btnClick(createAccountBtn, formLoginSubmit);
